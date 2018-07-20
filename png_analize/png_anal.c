e7152014b24c5d9e9b1f0ed8e51762fe0739fded
Ardameon <tyryshkinmv@gmail.com>
2018-07-20 19:16:35 +0700

    png_anal: zlib decompression added

--- png_analize/png_anal.c
+++ png_analize/png_anal.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 #include <errno.h>
 
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "zlib.h"
 
 #include <arpa/inet.h>
 
@@ -15,6 +17,9 @@
 #define IEND_STR "IEND"
 
 #define FILE_NAME "ByteByByte.png"
+
+#define CODE_BUF_LEN 4000000
+#define DECODE_BUF_LEN 15000000
 
 typedef struct {
 	uint32_t width;
@@ -33,6 +38,63 @@ typedef struct {
 	uint8_t  alfa;
 }pixel_t;
 
+
+void *myalloc(void *q, unsigned n, unsigned m)
+{
+    q = Z_NULL;
+    return calloc(n, m);
+}
+
+void myfree(void *q, void *p)
+{
+    q = Z_NULL;
+    free(p);
+}
+
+static alloc_func zalloc = myalloc;
+static free_func zfree = myfree;
+
+int uncompress_image(uint8_t *compr, uint32_t compr_len, uint8_t *uncompr, uint32_t uncompr_len, uint32_t *total_out)
+{
+    int err;
+    z_stream d_stream; /* decompression stream */
+
+    d_stream.zalloc = zalloc;
+    d_stream.zfree = zfree;
+    d_stream.opaque = (voidpf)0;
+
+    d_stream.next_in  = compr;
+    d_stream.avail_in = 0;
+    d_stream.next_out = uncompr;
+
+    printf("zstream format: %02x %02x\n", compr[0], compr[1]);
+
+    err = inflateInit(&d_stream);
+    if (err != Z_OK) return -1;
+
+//    d_stream.avail_in = compr_len;
+//    d_stream.avail_out = uncompr_len;
+
+
+    while (d_stream.total_out < uncompr_len && d_stream.total_in < compr_len - 1)
+    {
+        d_stream.avail_in = d_stream.avail_out = 1;
+        err = inflate(&d_stream, Z_NO_FLUSH);
+        if (err == Z_STREAM_END) break;
+        if (err != Z_OK)
+        {
+            printf("Total error(%d) in:%lu out:%lu\n", err, d_stream.total_in, d_stream.total_out);
+            return -2;
+        }
+    }
+
+    *total_out = d_stream.total_out;
+
+    err = inflateEnd(&d_stream);
+    if (err != Z_OK) return -3;
+
+    return 0;
+}
 
 int find_chunk(int fd, char *chunk_name, uint32_t *chunk_len)
 {
@@ -96,13 +158,49 @@ int find_code(int fd, uint32_t chunk_len)
 	return 0;
 }
 
+int find_code2(uint8_t *image_data, uint32_t size)
+{
+	uint8_t bitmap[65535];
+	int cur_len = 0, i;
+	pixel_t *pixel;
+	int cur_pos = 0;
+
+	while (cur_pos < size)
+	{
+        pixel = (pixel_t *)(image_data + cur_pos);
+
+		if (((pixel->red & 0x01) == (pixel->green & 0x01)) && ((pixel->blue & 0x01) == (pixel->green & 0x01)))
+		{
+			bitmap[cur_len++] = pixel->red & 0x01;
+		} else {
+			if (cur_len > 64 && !(cur_len % 8))
+			{
+				for (i = 0; i < cur_len; i++)
+				{
+					printf("%d", bitmap[i]);
+				}
+
+				printf(" (%d)\n", cur_len);
+			}
+
+			cur_len = 0;
+		}
+
+        cur_pos += sizeof(pixel_t);
+	}
+
+	return 0;
+}
+
 int main(int argc, char const *argv[])
 {
 	int fd = open(FILE_NAME, O_RDONLY);
 	png_ihdr_t ihdr = {0};
 	uint32_t chunk_len;
-	int i = 0;
-	
+    uint8_t *coded_data = NULL, *decoded_data = NULL;
+    uint32_t coded_len = 0, decoded_len = 0;
+	int i = 0, res = 0;
+
 	if (fd < 0)
 	{
 		perror("Failed to open file");
@@ -145,14 +243,32 @@ int main(int argc, char const *argv[])
 	// 	goto _exit_close;
 	// }
 
+    coded_data = malloc(CODE_BUF_LEN);
+    decoded_data = malloc(DECODE_BUF_LEN);
+
 	for (i = 0; !find_chunk(fd, IDAT_STR, &chunk_len); i++)
 	{
 		printf("%02d) IDAT len: %u\n", i, chunk_len);
 		
-		find_code(fd, chunk_len);
+//		find_code(fd, chunk_len);
+
+        coded_len += read(fd, coded_data + coded_len, chunk_len);
 	}
 
+    printf("total len: %u\n", coded_len);
+
+    if ((res = uncompress_image(coded_data, coded_len, decoded_data, DECODE_BUF_LEN, &decoded_len)))
+    {
+        printf("Uncopress error: %d\n", res);
+        goto _exit_close;
+    }
+
+    find_code2(decoded_data, decoded_len);
+
 _exit_close:
+
+    free(coded_data);
+    free(decoded_data);
 
 	close(fd);
 
