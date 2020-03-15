4040515b64ffb9eab2d2220ceeef58933bf56e44
Ardameon <helldiver@yandex.ru>
2020-03-15 19:12:38 +0700

    study: unit coverage report generation added

--- unit/src/tests/run.sh
+++ unit/src/tests/run.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-make -C .. all
-make
-./tests -c -v
+PARAM=$1
+
+case "$PARAM" in
+    "cov" | "coverage" )
+        make -C .. clean
+        CFLAGS="-fprofile-arcs -ftest-coverage -fPIC -O0" make -C .. all
+        make clean
+        make cov
+        ./tests -c -v
+        gcovr -r ..
+        ;;
+    * )
+        make -C .. clean
+        make -C .. all
+        make clean
+        make
+        ./tests -c -v
+        ;;
+esac
+
+
