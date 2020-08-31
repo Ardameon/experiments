3e2abad28ab99a362140162d6b31985f6ae7298e
Ardameon <tyryshkinmv@gmail.com>
2020-08-31 19:19:42 +0700

    study: cpp SPrata added

---
+++ study/cpp/SPrata/c10_classes_base/test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstring>
+#include <cstdint>
+
+using namespace std;
+
+class TestClass
+{
+    private:
+        int privateVar;
+        int privateMethod();
+
+    public:
+        int publicVar;
+        int publicMethod();
+
+    protected:
+        int protectedVar;
+        int protectedMethod();
+        
+};
+
+int TestClass::publicMethod()
+{
+    return 2;
+}
+
+inline char *inline_func()
+{
+    char *a = new char[20];
+
+    strcpy(a, "Dota");
+
+    return a;
+}
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	(void)argv;
+
+    TestClass obj;
+    uint32_t a;
+
+    cout << "Hello world\n" << obj.publicMethod() << '\n' << inline_func();
+
+    a = obj.publicVar = 2;
+
+    return a;
+}
