9f032a870b24f262b7e4f39b2ea748530a7dfd95
Ardameon <helldiver@yandex.ru>
2020-11-01 18:44:14 +0700

    study: cpp alg MList class nehanced (find, erase, getMiddle, reverse, copy)

--- study/cpp/algorythms/a02_LinkedList/main.cpp
+++ study/cpp/algorythms/a02_LinkedList/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include "mlist.h"
 
 using namespace std;
@@ -6,8 +7,31 @@ using namespace std;
 int main()
 {
     MList<int> l = {1, 2, 3, 4, 5};
+    MList<int> l1 = MList<int>(10);
+    MList<int> l2;
 
-    cout << l << endl;
+    srand(time(0));
+
+    cout << std::boolalpha;
+    cout << l << " (" << l.Size() << ")" << endl;
+
+    cout << "Find  2: " << l.Find(2) << endl;
+    cout << "Find -2: " << l.Find(-2) << endl;
+
+    l.Erase(-5);
+
+    cout << l << " mid: " << l.GetMiddle() << endl;
+    l.Erase(2);
+    cout << l << " mid: " << l.GetMiddle() << endl;
+    l.Erase(1);
+    cout << l << " mid: " << l.GetMiddle() << endl;
+    l.Erase(5);
+    cout << l << " mid: " << l.GetMiddle() << endl;
+
+    cout << l1 << " mid: " << l1.GetMiddle() << endl;
+    cout << l1.Reverse() << " mid: " << l1.GetMiddle() << endl;
+    cout << l2 << " (" << l2.Size() << ")" << endl;
+    cout << (l2 = l1) << " (" << l2.Size() << ")" << endl;
 
     return 0;
 }
--- study/cpp/algorythms/a02_LinkedList/mlist.h
+++ study/cpp/algorythms/a02_LinkedList/mlist.h
@@ -23,21 +23,30 @@ class MList
 {
 public:
     MList();
+    MList(int size);
     MList(std::initializer_list<T> l);
+    MList(const MList<T> &list);
     ~MList();
 
     size_t Size() const;
 
-    int PushFront(const T &data);
-    int PushBack(const T &data);
+    int  PushFront(const T &data);
+    int  PushBack(const T &data);
+    bool Find(const T &data) const;
+    int  Erase(const T &data);
+    T    GetMiddle() const;
+    MList<T> &Reverse();
 
     template <typename ST>
     friend std::ostream & operator << (std::ostream &os, const MList<ST> &list);
+
+    MList<T> &operator =(const MList<T> &list);
 
 private:
 
     Node<T> *NodeAllocate(const T &data) const {return new Node<T>(data);}
     void     NodeFree(Node<T> *node) const {delete node;}
+
 
     Node<T>  *head_;
 };
@@ -53,11 +62,35 @@ MList<T>::MList() : head_(nullptr)
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 template <typename T>
+MList<T>::MList(int size) : head_(nullptr)
+{
+    while (size--)
+    {
+        PushBack((rand() % 50 - 25));
+    }
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+template <typename T>
 MList<T>::MList(std::initializer_list<T> l) : head_(nullptr)
 {
     for (auto &item : l)
     {
         PushBack(item);
+    }
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+template <typename T>
+MList<T>::MList(const MList<T> &list) : head_(nullptr)
+{
+    Node<T> *cur = list.head_;
+
+    while (cur)
+    {
+        PushBack(cur->data);
     }
 }
 
@@ -133,6 +166,136 @@ int MList<T>::PushBack(const T &data)
     }
 
     return 0;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+template <typename T>
+bool MList<T>::Find(const T &data) const
+{
+    Node<T> *cur = head_;
+
+    while (cur)
+    {
+        if (cur->data == data)
+            return true;
+
+        cur = cur->next;
+    }
+
+    return false;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+template <typename T>
+int MList<T>::Erase(const T &data)
+{
+    Node<T> *prev, *cur = head_;
+    int res = -1;
+
+    if (!head_) goto _exit;
+
+    if (head_->data == data)
+    {
+        cur = head_;
+        head_ = head_->next;
+
+        NodeFree(cur);
+        res = 0;
+        goto _exit;
+    }
+
+    cur = head_->next;
+    prev = head_;
+
+    while (cur)
+    {
+        if (cur->data == data)
+        {
+            prev->next = cur->next;
+            NodeFree(cur);
+            res = 0;
+            goto _exit;
+        }
+
+        prev = cur;
+        cur = cur->next;
+    }
+
+_exit:
+    return res;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+template<typename T>
+T MList<T>::GetMiddle() const
+{
+    int mid_idx = (Size() / 2) + 1;
+    Node<T> *cur = head_;
+
+    if (!cur) return 0;
+
+    if (!cur->next) return cur->data;
+
+    while (--mid_idx)
+    {
+        cur = cur->next;
+    }
+
+    return cur->data;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+template <typename T>
+MList<T> &MList<T>::Reverse()
+{
+    Node<T> *prev, *cur, *next;
+
+    if (!head_) goto _exit;
+
+    cur = head_->next;
+    prev = head_;
+    prev->next = nullptr;
+
+    while (cur)
+    {
+        next = cur->next;
+        cur->next = prev;
+
+        prev = cur;
+        cur = next;
+    }
+
+    head_ = prev;
+
+_exit:
+    return *this;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+template<typename T>
+MList<T> &MList<T>::operator =(const MList<T> &list)
+{
+    Node<T> *cur;
+
+    if (this == &list)
+        return *this;
+
+    while (!Erase(head_->data));
+
+    cur = list.head_;
+
+    while (cur)
+    {
+        PushBack(cur->data);
+        cur = cur->next;
+    }
+
+    return *this;
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
