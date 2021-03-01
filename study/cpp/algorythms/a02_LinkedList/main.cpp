25f234bec92a0fa443c48b9fec1303ac68a3fcba
Ardameon <helldiver@yandex.ru>
2021-03-02 03:35:29 +0700

    study: cpp alg SimpleList class added as alternative to MList

--- study/cpp/algorythms/a02_LinkedList/main.cpp
+++ study/cpp/algorythms/a02_LinkedList/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include "simplelist.h"
 #include "mlist.h"
 
 using std::cout;
@@ -8,6 +9,7 @@ using std::endl;
 
 int main()
 {
+#if 0
     MList<int> l = {1, 2, 3, 4, 5};
     MList<int> l1 = MList<int>(10);
     MList<int> l2;
@@ -37,6 +39,37 @@ int main()
     cout << l1.Reverse() << " mid: " << l1.GetMiddle() << endl;
     cout << l2 << " (" << l2.Size() << ")" << endl;
     cout << (l2 = l1) << " (" << l2.Size() << ")" << endl;
+#else
+    SimpleList l = {1, 2, 3, 4, 5};
+    SimpleList l1 = SimpleList(10);
+    SimpleList l2;
+    SimpleList l3 = l1;
+
+    srand(time(0));
+
+    cout << std::boolalpha;
+    cout << l << " (" << l.Size() << ")" << endl;
+
+    cout << "Find  2: " << (l.Find(2) ? true : false) << endl;
+    cout << "Find -2: " << (l.Find(-2) ? true : false) << endl;
+
+    l.Erase(l.Find(-5));
+
+    cout << l << " mid: " << l.GetMiddle()->data << endl;
+    l.Erase(l.Find(2));
+    cout << l << " mid: " << l.GetMiddle()->data << endl;
+    l.Erase(l.Find(1));
+    cout << l << " mid: " << l.GetMiddle()->data << endl;
+    l.Erase(l.Find(5));
+    cout << l << " mid: " << l.GetMiddle()->data << endl;
+
+    cout << "=== l1 ===" << endl;
+
+    cout << l1 << " mid: " << l1.GetMiddle()->data << endl;
+    cout << l1.Reverse() << " mid: " << l1.GetMiddle()->data << endl;
+    cout << l2 << " (" << l2.Size() << ")" << endl;
+    cout << (l2 = l1) << " (" << l2.Size() << ")" << endl;
+#endif
 
     return 0;
 }
