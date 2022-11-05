5fce5f3ef93c27c66a446222dc942d0bb2ced528
Ardameon <helldiver@yandex.ru>
2022-11-05 14:18:21 +0700

    study: patterns c.4 SaladShop destructor enhanced

--- study/cpp/PatternsFreeman/4_Factory/salad_shop.h
+++ study/cpp/PatternsFreeman/4_Factory/salad_shop.h
@@ -1,6 +1,8 @@
 #ifndef SALAD_SHOP_H
 #define SALAD_SHOP_H
 
+#include <iostream>
+
 #include "factory.h"
 #include "salad.h"
 
@@ -19,7 +21,10 @@ protected:
 
 public:
     SaladShop(IIngredientFactory *factory) : _factory(factory) {}
-    virtual ~SaladShop() {}
+    virtual ~SaladShop() 
+    {
+        std::cout << "SaladShop Destroyed" << std::endl;
+    }
 
     virtual ISalad *CreateSalad(SaladType type)
     {
@@ -59,6 +64,14 @@ class MoscowSaladShop : public SaladShop
 {
 public:
     MoscowSaladShop() : SaladShop(new MoscowIngredientFactory) {}
+    virtual ~MoscowSaladShop() 
+    {
+        if (_factory)
+        {
+            std::cout << "Moscow SaladShop Destroyed" << std::endl;
+            delete _factory;
+        }
+    }
 };
 
 
@@ -68,6 +81,14 @@ class NovosibirskSaladShop : public SaladShop
 {
 public:
     NovosibirskSaladShop() : SaladShop(new NovosibirskIngredientFactory) {}
+    virtual ~NovosibirskSaladShop() 
+    {
+        if (_factory)
+        {
+            std::cout << "Novosibirsk SaladShop Destroyed" << std::endl;
+            delete _factory;
+        }
+    }
 };
 
 /* ================================================================================================================= */
