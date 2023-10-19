b1caec5787bb0056400d1f761708d345e884d94a
Ardameon <tyryshkinmv@gmail.com>
2023-10-19 12:08:50 +0700

    tests: bit union updated

--- test/bit_union.c
+++ test/bit_union.c
@@ -25,7 +25,15 @@ typedef struct
                      a15:1,
                      :0;
         };
+        struct {
+            uint8_t alarm_0:1,
+                    alarm_1:1,
+                    alarm_2:1,
+                    alarm_3:1,
+                           :0;
+        };
         uint16_t mask;
+        uint8_t  mask_byte[2];
     } u;
 } bit_un_t;
 
@@ -33,23 +41,31 @@ int main()
 {
     bit_un_t st;
 
-    /* 0xA */
-    st.u.a1 = 1;
-    st.u.a3 = 1;
-    /* 0xB */
-    st.u.a4 = 1;
-    st.u.a5 = 1;
-    st.u.a7 = 1;
-    /* 0xC */
-    st.u.a10 = 1;
-    st.u.a11 = 1;
-    /* 0xC */
-    st.u.a12 = 1;
-    st.u.a14 = 1;
-    st.u.a15 = 1;
+    if (0)
+    {
+        /* 0xA */
+        st.u.a1 = 1;
+        st.u.a3 = 1;
+        /* 0xB */
+        st.u.a4 = 1;
+        st.u.a5 = 1;
+        st.u.a7 = 1;
+        /* 0xC */
+        st.u.a10 = 1;
+        st.u.a11 = 1;
+        /* 0xC */
+        st.u.a12 = 1;
+        st.u.a14 = 1;
+        st.u.a15 = 1;
+    } else {
+        /* 0x9 */
+        st.u.alarm_0 = 1;
+        st.u.alarm_3 = 1;
+    }
 
-
-    printf("union value: %#x\n", st.u.mask);
+    printf("union value: %#06x\n", st.u.mask);
+    printf("union value: byte[0] = %#04x\n", st.u.mask_byte[0]);
+    printf("union value: byte[1] = %#04x\n", st.u.mask_byte[1]);
 
     return 0;
 }
