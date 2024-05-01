8d3dce277a534a4ec766d86e8c42875bbe1c0408
Ardameon <helldiver@yandex.ru>
2024-05-01 23:23:42 +0700

    study: patterns c.1 Strategy c-implementation post tests fixes

--- study/cpp/PatternsFreeman/1_Ducks/c_imp/duck.c
+++ study/cpp/PatternsFreeman/1_Ducks/c_imp/duck.c
@@ -4,25 +4,30 @@
 
 void duck_create(duck_t *duck)
 {
-    memset(duck, 0, sizeof(*duck));
+    if (duck)
+        memset(duck, 0, sizeof(*duck));
 }
 
 void duck_destroy(duck_t *duck)
 {
-    duck->duck_destroy(duck);
+    if (duck && duck->duck_destroy)
+        duck->duck_destroy(duck);
 }
 
 void duck_show(duck_t *duck)
 {
-    duck->duck_show();
+    if (duck && duck->duck_show)
+        duck->duck_show();
 }
 
 void duck_fly(duck_t *duck)
 {
-    duck->fly_behaviour.fly();
+    if (duck && duck->fly_behaviour.fly)
+        duck->fly_behaviour.fly();
 }
 
 void duck_quak(duck_t *duck)
 {
-    duck->quak_behaviour.quak();
+    if (duck && duck->quak_behaviour.quak)
+        duck->quak_behaviour.quak();
 }
