6727bbdb022c8ab2e726b67edb45a338c4ddc84d
Ardameon <helldiver@yandex.ru>
2026-03-14 03:35:32 +0700

    study: python Berry exercises

--- study/python/PythonBerry/my_tests/exercises_2026_02_23/ex_08_quiz.py
+++ study/python/PythonBerry/my_tests/exercises_2026_02_23/ex_08_quiz.py
@@ -4,3 +4,44 @@
 # Пройдись по вопросам, запрашивая ответ у пользователя (input()).
 # Подсчитай количество правильных ответов.
 # Подсказка: используй enumerate() или zip() для прохода по двум спискам
+
+import random
+
+questions = [
+    "В каком году я родился",
+    "Как звали следопыта из Властелина колец",
+    "Где живут пингвины",
+    "Как зовут мою собаку",
+    "Когда у меня день рождения"
+]
+
+answers = [
+    "1991",
+    "Арагорн",
+    "В антарктике",
+    "Мышка",
+    "13 мая"
+]
+
+def quiz(questions_answer_list):
+    (i, (question, answer)) = random.choice(questions_answer_list)
+
+    print(f"{i}. {question}?")
+    user_answer = input("Введите ответ: ")
+
+    if (answer.lower() == user_answer.lower()):
+        print("Верно!")
+    else:
+        print("Неверно! Правильный ответ -", answer)
+
+# enumerate returns iterator, so it will "disappear" after iteration finished
+quiz_enum = enumerate(zip(questions, answers), 1)
+
+# for i, (question, answer) in quiz_enum:
+#     print(f"{i}) {question} - {answer}")
+
+quiz_list = list(enumerate(zip(questions, answers), 1))
+
+quiz(quiz_list)
+
+
--- study/python/PythonBerry/my_tests/exercises_2026_02_23/ex_09_password_gen.py
+++ study/python/PythonBerry/my_tests/exercises_2026_02_23/ex_09_password_gen.py
@@ -6,3 +6,17 @@
 # 3. Собирает их в строку через join()
 # 4. Выводит готовый пароль
 # Подсказка: используй random.choice() в цикле или random.sample()
+
+import random
+import string
+
+symbols = string.ascii_letters + string.digits + string.punctuation
+
+# password = "".join(random.sample(symbols, 12))
+
+password = ""
+
+for i in range(12):
+    password += random.choice(symbols)
+
+print(password)
